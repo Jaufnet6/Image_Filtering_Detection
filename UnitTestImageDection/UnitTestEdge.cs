019d0b5304a3acd0ead477be523cbd712872a102
Jaufnet6 <jaufray.sornette@gmail.com>
2018-10-04 15:35:27 +0200

    Photo Added

--- UnitTestImageDection/UnitTestEdge.cs
+++ UnitTestImageDection/UnitTestEdge.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace UnitTestImageDection
@@ -6,6 +8,17 @@ namespace UnitTestImageDection
     [TestClass]
     public class UnitTestEdge
     {
+        private Bitmap testBitmap = null;
+        private static string FileName = "test.png";
+        StreamReader streamReader = new StreamReader(FileName);
+
+        public void InitializeTestPic()
+        {
+            testBitmap = (Bitmap)Bitmap.FromStream(streamReader.BaseStream);
+            streamReader.Close();
+        }
+       
+
         [TestMethod]
         public void TestMethod1()
         {
