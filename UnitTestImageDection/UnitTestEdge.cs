56eda4236aa4bb006341bda1e4b1ef49b14a753a
Jaufnet6 <jaufray.sornette@gmail.com>
2018-10-23 16:42:22 +0200

    Copy to canvas method tested

--- UnitTestImageDection/UnitTestEdge.cs
+++ UnitTestImageDection/UnitTestEdge.cs
@@ -60,6 +60,146 @@ namespace UnitTestImageDection
 
         }
 
+        //Testing the black result when sending a white images
+        [TestMethod]
+        public void TestBlackWhenSentWhiteImage()
+        {
+            //Create white Bitmap
+            Bitmap whiteBPM = new Bitmap(100, 100);
+            using (Graphics gfx = Graphics.FromImage(whiteBPM))
+            using (SolidBrush brush = new SolidBrush(Color.FromArgb(255, 255, 255)))
+            {
+                gfx.FillRectangle(brush, 0, 0, 100, 100);
+            }
+
+            //Create black Bitmap
+            Bitmap blackBPM = new Bitmap(100, 100);
+            using (Graphics gfx = Graphics.FromImage(blackBPM))
+            using (SolidBrush brush = new SolidBrush(Color.FromArgb(0, 0, 0)))
+            {
+                gfx.FillRectangle(brush, 0, 0, 100, 100);
+            }
+
+            //Send white Bitmap to laplacian
+            Bitmap testResult = ExtBitmap.Laplacian3x3Filter(whiteBPM, false);
+
+            //Compare same pixel that should have changed to black
+            Assert.AreEqual(blackBPM.GetPixel(2,2), testResult.GetPixel(2,2));
+
+
+        }
+
+
+        //Test when sending a bitmap with superior width that canvas and height
+        [TestMethod]
+        public void TestCopyToSquareCanvasWithWidthGreaterThatCanvasAndHeight()
+        {
+            //Initialize sizes
+            float bpmHeight = 200f;
+            float bpmWidth = 1000f;
+            int canvasSize = 400;
+
+            //Determine the ratio for expected results
+            float ratio = bpmWidth / canvasSize;
+            
+            //Send bitmap with height, width and canvas to tested method
+            Bitmap bpmAfterMethodCanvas = ExtBitmap.CopyToSquareCanvas (new Bitmap((int)bpmWidth, (int)bpmHeight), canvasSize);
+
+            //All expected sizes
+            float bpmWidthExpected = canvasSize;
+            float bpmHeightExpected = bpmHeight / ratio;
+            //Tested sizes
+            float widthResult = (float) bpmAfterMethodCanvas.Width;
+            float heigthResult = (float) bpmAfterMethodCanvas.Height;
+            //Compare values
+            Assert.AreEqual(bpmWidthExpected, widthResult);
+            Assert.AreEqual(bpmHeightExpected, heigthResult);
+
+        }
+
+        //Test when sending a bitmap with superior height that canvas and width
+        [TestMethod]
+        public void TestCopyToSquareCanvasWithHeightGreaterThatCanvasAndWidth()
+        {
+            //Initialize sizes
+            float bpmHeight = 400f;
+            float bpmWidth = 200f;
+            int canvasSize = 100;
+
+            //Determine the ratio for expected results
+            float ratio = bpmHeight / canvasSize;
+
+            //Send bitmap with height, width and canvas to tested method
+            Bitmap bpmAfterMethodCanvas = ExtBitmap.CopyToSquareCanvas(new Bitmap((int)bpmWidth, (int)bpmHeight), canvasSize);
+
+            //All expected sizes
+            float bpmWidthExpected = bpmWidth / ratio;
+            float bpmHeightExpected = canvasSize;
+            //Tested sizes
+            float widthResult = (float)bpmAfterMethodCanvas.Width;
+            float heigthResult = (float)bpmAfterMethodCanvas.Height;
+            //Compare values
+            Assert.AreEqual(bpmWidthExpected, widthResult);
+            Assert.AreEqual(bpmHeightExpected, heigthResult);
+
+        }
+
+        //Test when sending a bitmap with hight and width same size as the canvas
+        [TestMethod]
+        public void TestCopyToSquareCanvasWithAllSameSize()
+        {
+            //Initialize sizes
+            float bpmHeight = 200f;
+            float bpmWidth = 200f;
+            int canvasSize = 200;
+
+            //Determine the ratio for expected results
+            float ratio = bpmWidth / canvasSize;
+
+            //Send bitmap with height, width and canvas to tested method
+            Bitmap bpmAfterMethodCanvas = ExtBitmap.CopyToSquareCanvas(new Bitmap((int)bpmWidth, (int)bpmHeight), canvasSize);
+
+            //All expected sizes
+            float bpmWidthExpected = canvasSize;
+            float bpmHeightExpected = bpmHeight / ratio;
+            //Tested sizes
+            float widthResult = (float)bpmAfterMethodCanvas.Width;
+            float heigthResult = (float)bpmAfterMethodCanvas.Height;
+            //Compare values
+            Assert.AreEqual(bpmWidthExpected, widthResult);
+            Assert.AreEqual(bpmHeightExpected, heigthResult);
+
+        }
+
+        //Test when sending a bitmap with canvas smaller that height and width (both same size)
+        [TestMethod]
+        public void TestCopyToSquareCanvasWithSamllerCanvas()
+        {
+            //Initialize sizes
+            float bpmHeight = 200f;
+            float bpmWidth = 200f;
+            int canvasSize = 100;
+
+            //Determine the ratio for expected results
+            float ratio = bpmWidth / canvasSize;
+
+            //Send bitmap with height, width and canvas to tested method
+            Bitmap bpmAfterMethodCanvas = ExtBitmap.CopyToSquareCanvas(new Bitmap((int)bpmWidth, (int)bpmHeight), canvasSize);
+
+            //All expected sizes
+            float bpmWidthExpected = canvasSize;
+            float bpmHeightExpected = bpmHeight / ratio;
+            //Tested sizes
+            float widthResult = (float)bpmAfterMethodCanvas.Width;
+            float heigthResult = (float)bpmAfterMethodCanvas.Height;
+            //Compare values
+            Assert.AreEqual(bpmWidthExpected, widthResult);
+            Assert.AreEqual(bpmHeightExpected, heigthResult);
+
+        }
+
+
+
 
     }
 }
